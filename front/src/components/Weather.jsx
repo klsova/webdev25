e32c7da90fa42ccf2f8da758ed06020b160cac4a
klsova <kane.sova@gmail.com>
2025-03-11 14:47:55 +0200

    index page init

---
+++ front/src/components/Weather.jsx
@@ -0,0 +1,11 @@
+import './Weather.css';
+
+const Weather = () => {
+    return (
+        <div className='weather-box'>
+            <h1>Tähän tulee sää</h1>
+        </div>
+    );
+}
+
+export default Weather;
