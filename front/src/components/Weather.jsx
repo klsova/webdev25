4a40fd7407c04b6df3d98d3d42d27b5c7d7fa7bf
klsova <kane.sova@gmail.com>
2025-04-02 13:07:48 +0300

    weather background placeholder

--- front/src/components/Weather.jsx
+++ front/src/components/Weather.jsx
@@ -1,8 +1,29 @@
 import './Weather.css';
+import { useState, useEffect } from 'react';
+
+function setBackground(weather) {
+    if (weather === "insert weather type") {
+        document.body.style.backgroundImage = './assets/rain.jpg'
+    } else if (weather === "insert weather type") {
+        document.body.style.backgroundImage = './assets/snow.jpg'
+    } else if (weather === "insert weather type") {
+        document.body.style.backgroundImage = './assets/sunny.jpg'
+    } else if (weather === "insert weather type") {
+        document.body.style.backgroundImage = './assets/cloudy.jpg'
+    }
+}
 
 const Weather = () => {
+
+    setBackground('weather')
+    
     return (
-        <div className='weather-box'>
+        <div className='weather-box'
+        style={{
+            backgroundImage: `url(${background})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+        }}>
             <h1>Turku</h1>
             <div className='day1'>Ma</div>
             <div className='day2'>Ti</div>
@@ -12,6 +33,7 @@ const Weather = () => {
             <div className='day6'>La</div>
             <div className='day7'>Su</div>
         </div>
+
     );
 }
 
